fbceca76449c9cddabad7a6660ac79f78696479d
mihaicce <cocea.mike@gmail.com>
2023-07-12 13:37:10 +0200

    pagina de admin echa pero las tablas no se hacen responsive

---
+++ src/components/card/FormProductos.vue
@@ -0,0 +1,161 @@
+<template>
+    <div class="product-form justify-content-around shadow-box">
+      <form @submit="submitForm" class="d-flex">
+        <div class="form-column">
+          <div class="form-group">
+            <!-- <label for="nombre">Nombre</label> -->
+            <input type="text" id="nombre" v-model="nombre" placeholder="Nombre" required>
+          </div>
+          <div class="form-group">
+            <!-- <label for="descripcion">Descripción:</label> -->
+            <textarea id="descripcion" v-model="descripcion" placeholder="Descripcion" required></textarea>
+          </div>
+          <div class="form-group">
+            <!-- <label for="foto">Foto:</label> -->
+            <input type="file" id="foto" accept="image/*" @change="handleFileUpload" required>
+          </div>
+        </div>
+        <div class="form-column">
+          <div class="form-group">
+            <!-- <label for="vendedor">Vendedor:</label> -->
+            <input type="text" id="vendedor" v-model="vendedor" placeholder="Vendedor" required>
+          </div>
+          <div class="form-group">
+            <!-- <label for="precio">Precio:</label> -->
+            <input type="number" id="precio" v-model="precio" placeholder="Precio" required>
+          </div>
+          <div class="form-actions">
+            <button type="submit">Enviar</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  
+  const nombre = ref('');
+  const descripcion = ref('');
+  const foto = ref(null);
+  const vendedor = ref('');
+  const precio = ref('');
+  
+  function handleFileUpload(event) {
+    const file = event.target.files[0];
+    if (file) {
+      foto.value = file;
+    }
+  }
+  
+  function submitForm(event) {
+    event.preventDefault();
+  
+    // Aquí puedes agregar la lógica para enviar el formulario
+  }
+  </script>
+  
+  <style>
+  .product-form {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+  
+  .form-column {
+    width: 100%;
+    max-width: 400px;
+    padding: 0 16px;
+    box-sizing: border-box;
+  }
+  
+  .form-group {
+    margin-bottom: 16px;
+  }
+  
+  /* label {
+    display: block;
+    margin-bottom: 4px;
+  }
+  
+  input,
+  textarea {
+    width: 100%;
+    padding: 8px;
+    border: none;
+    border-radius: 0.8rem;
+  } */
+
+  /* label {
+  position: absolute;
+  transform: translateY(6px);
+  left: 13px;
+  color: rgba(255, 255, 255, 0.5);
+  transition: all 0.25s ease;
+  -webkit-backface-visibility: hidden;
+  pointer-events: none;
+  font-size: 22px;
+} */
+
+label .req {
+  margin: 2px;
+  color: #1ab188;
+}
+
+label.active {
+  transform: translateY(50px);
+  left: 2px;
+  font-size: 14px;
+}
+
+label.active .req {
+  opacity: 0;
+}
+
+label.highlight {
+  color: #fff;
+}
+
+input, textarea {
+  font-size:medium;
+  display: block;
+  width: 100%;
+  height: 100%;
+  padding: 5px 10px;
+  background: none;
+  background-image: none;
+  border: 1px solid #ddd;
+  border-top: none;
+  border-left: none;
+  border-right: none;
+  color: #000000;
+  border-radius: 0;
+  transition: all 0.5s ease;
+  border-radius: 5px;
+}
+
+input:focus {
+  outline: none;
+  border-color: #1ab188;
+}
+  
+  .form-actions {
+    width: 100%;
+    text-align: center;
+    margin-top: 16px;
+  }
+  
+  button {
+    padding: 8px 16px;
+    background-color: #005858;
+    color: #fff;
+    border: none;
+    cursor: pointer;
+    border-radius: 0.8rem;
+  }
+  
+  button:hover {
+    background-color: #5FC74B;
+  }
+  
+  </style>
---
+++ src/components/card/TablaUsuario.vue
@@ -0,0 +1,113 @@
+<template>
+    <div class="user-table todo">
+      <table>
+        <thead>
+          <tr>
+            <th>Foto</th>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Apellido</th>
+            <th>Email</th>
+            <th>Teléfono</th>
+            <th>Sector Peñascales</th>
+            <th>Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(user, index) in users" :key="index">
+            <td><img :src="user.foto" alt="Foto del usuario"></td>
+            <td>{{ user.id }}</td>
+            <td>{{ user.nombre }}</td>
+            <td>{{ user.apellido }}</td>
+            <td>{{ user.email }}</td>
+            <td>{{ user.telefono }}</td>
+            <td>{{ user.sector }}</td>
+            <td>
+              <span @click="editarUsuario(index)"><i class="fas fa-edit"></i></span>
+              <span @click="borrarUsuario(index)"><i class="fas fa-trash"></i></span>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  
+  const users = ref([
+    {
+      foto: 'ruta/a/foto1.jpg',
+      id: 1,
+      nombre: 'Nombre 1',
+      apellido: 'Apellido 1',
+      email: 'correo1@example.com',
+      telefono: '123456789',
+      sector: 'Peñascales 1'
+    },
+    {
+      foto: 'ruta/a/foto2.jpg',
+      id: 2,
+      nombre: 'Nombre 2',
+      apellido: 'Apellido 2',
+      email: 'correo2@example.com',
+      telefono: '987654321',
+      sector: 'Peñascales 2'
+    },
+    // Agrega más usuarios aquí
+  ]);
+  
+  function editarUsuario(index) {
+    // Lógica para editar el usuario seleccionado
+  }
+  
+  function borrarUsuario(index) {
+    // Lógica para borrar el usuario seleccionado
+  }
+  </script>
+  
+  <style>
+  .todo {
+    margin-left: 2rem;
+    margin-bottom: 2rem;
+  }
+  
+  .user-table {
+    width: 100%;
+  }
+  
+  table {
+    width: 95%;
+    border-collapse: collapse;
+  }
+  
+  th,
+  td {
+    padding: 8px;
+    border-bottom: 1px solid #ddd;
+    text-align: left;
+  }
+  
+  th {
+    background-color: #005858;
+    color: white;
+  }
+  
+  tr {
+    background-color: #1ab188;
+    color: white
+  }
+  
+  i {
+    cursor: pointer;
+    margin-left: 8px;
+  }
+  
+  .fa-edit:hover {
+    color: #5FC74B;
+  }
+  
+  .fa-trash:hover {
+    color: red;
+  }
+  </style>
