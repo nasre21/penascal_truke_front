5afd97c4193857e8f599ab5d7831a5433c841137
nasre21 <nasou2001@gmail.com>
2023-07-17 14:21:25 +0200

    trying get up imagenes

--- src/components/card/CatalogoImagines.vue
+++ src/components/card/CatalogoImagines.vue
@@ -1,20 +1,21 @@
 <template>
-<section class="gallery">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-4" v-for="data in dataInfo.data" :key="data.id">
-        <div class="card card_all">
-          <img src="https://images.unsplash.com/photo-1477862096227-3a1bb3b08330?ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60" alt="" class="card-img-top">
-          <div class="card-body d-flex justify-content-around">
-            <h5 class="card-title">{{ data.name }}</h5>
-            <a href="" class="btn btn-outline-success btn-sm">Read More</a>
-            <a href="" class="btn btn-outline-danger btn-sm"><i class="far fa-heart"></i></a>
-          </div>
+<div class="container">
+  <div class="row">
+    <div class="col-md-4" v-for="data in dataInfo.data" :key="data.id">
+      <div class="card card_all">
+        <!-- <img v-for="photo in data.files.photos" :key="photo.name" :src="photo.dataURL" alt="" class="card-img-top"> -->
+        <div class="card-body d-flex justify-content-around">
+          <h5 class="card-title">{{ data.name }}</h5>
+          <a href="" class="btn btn-outline-success btn-sm">Read More</a>
+          <a href="" class="btn btn-outline-danger btn-sm"><i class="far fa-heart"></i></a>
         </div>
       </div>
+      {{ data.files }}
     </div>
   </div>
-</section>
+</div>
+
+
 
 </template>
 
