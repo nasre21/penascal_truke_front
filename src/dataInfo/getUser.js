49bc378fc04f6c25ea639e6c31bb9e95b3f05f86
mihaicce <cocea.mike@gmail.com>
2023-07-17 14:24:20 +0200

    getUser.js echo

---
+++ src/dataInfo/getUser.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+import {ref} from 'vue'
+
+
+async function getUser(){
+    let isError = false
+    let isLoading = true
+
+      let data = ref('')
+
+      try {
+        let answer = await axios.get("http://127.0.0.1:5000/users")
+        data.value = answer.data
+      }
+      catch (err) {
+        isError = true
+      }
+      isLoading = false;
+
+      return {isError, isLoading, data}
+}
+
+export default {getUser}
