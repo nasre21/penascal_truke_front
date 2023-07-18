39e5192d9881c0e4389a20ad749d76369003345d
MariaDiezEsteve <diezesteve@outlook.es>
2023-07-18 21:29:35 +0200

    incluidas las fotos en vue y visualizadas en localhost:8080

--- src/components/card/CatalogoImagines.vue
+++ src/components/card/CatalogoImagines.vue
@@ -3,13 +3,15 @@
     <div class="row">
       <div class="col-md-4" v-for="data in dataInfo.data" :key="data.id">
         <div class="card card_all">
-          <div v-for="(file, index) in data.files" :key="index">
-            <img :src="JSON.parse(files).photos[0].dataURL" alt="Imagen" class="card-img-top">
+          <!-- Mostrar las imágenes -->
+          <div v-for="file in parseFiles(data.files)" :key="file.id">
+            <img :src="file.url" alt="Imagen del producto" />
           </div>
+
           <div class="card-body d-flex justify-content-around">
             <h5 class="card-title">{{ data.name }}</h5>
             <p>{{ data.price }} Peñascales</p>
-            <a href="" class="btn btn-outline-success btn-sm" >Accede al producto</a>
+            <a href="" class="btn btn-outline-success btn-sm">Accede al producto</a>
           </div>
         </div>
       </div>
@@ -18,37 +20,22 @@
 </template>
 
 <script setup>
-import { defineProps, ref} from 'vue'
-const { dataInfo } = defineProps(['dataInfo'])
+import { defineProps, ref } from 'vue';
 
+const { dataInfo } = defineProps(['dataInfo']);
 
-console.log("esto es datainfoooooo", dataInfo.data[2].files)
-console.log("dato util", JSON.parse(dataInfo.data[2].files));
-console.log("dato util", JSON.parse(dataInfo.data[2].files).photos[0].dataURL);
-  
-let photo = {};
-
-for (let i = 0; i < dataInfo.data.length; i++) {
-  if (dataInfo.data[i].files) {
-    try {
-      photo.value = JSON.parse(dataInfo.data[i].files);
-      if (photo.value.photos && Array.isArray(photo.value.photos)) {
-        for (let j = 0; j < photo.value.photos.length; j++) {
-          const dataURL = photo.value.photos[j].dataURL;
-          console.log(`Imagen ${i + 1}, foto ${j + 1}: ${dataURL}`);
-          // Realizar cualquier otra acción que necesites con el dataURL de la imagen
-        }
-      }
-    } catch (error) {
-      console.error(`Error al analizar JSON en dataInfo.data[${i}].files: ${error}`);
-    }
+function parseFiles(filesString) {
+  try {
+    const filesArray = JSON.parse(filesString);
+    return filesArray.map((url, id) => ({ url, id }));
+  } catch (error) {
+    console.error("Error al analizar los archivos:", error);
+    return [];
   }
 }
-
-
-
-
 </script>
+
+
 
 <style scoped>
 
