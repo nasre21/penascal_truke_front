968e451364f2751474c608295214fed002e079c7
nasre21 <nasou2001@gmail.com>
2023-07-21 09:39:53 +0200

    vista carrito empezada

--- src/router/index.js
+++ src/router/index.js
@@ -46,7 +46,15 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/ProductoView.vue')
-  }
+  },
+  {
+    path: '/shop',
+    name: 'shop',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/CarritoView.vue')
+  },
 ]
 
 const router = createRouter({
