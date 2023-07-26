4005843e2a7a6e2e42ba546411349d0c73166819
nasre21 <nasou2001@gmail.com>
2023-07-26 13:22:48 +0200

    los cambios del buy

--- src/components/Tablas/tablaComprado.vue
+++ src/components/Tablas/tablaComprado.vue
@@ -1,85 +1,73 @@
 <template>
-    <div class="shadow-box" >
-   <div class="product-table todo">
-  <div class="table-wrapper">
+  <div class="shadow-box" >
+ <div class="product-table todo">
+<div class="table-wrapper">
+  <table>
+    <thead>
+      <tr>
+        <th>Comprado</th>
+        <th>Nombre</th>
+        <th>Descripción</th>
+        <th>Precio</th>
+       
+      </tr>
+    </thead>
+
+
+    <tbody>
+      <tr v-for="(product, index) in products" :key="index">
+        <td data-label="Comprado"><img :src="product.foto" alt="Foto del producto"></td>
+        <td data-label="Nombre">{{ product.nombre }}</td>
+        <td data-label="Descripción">{{ product.descripcion }}</td>
+        <td data-label="Precio">{{ product.precio }}</td>
+        
+      </tr>
+    </tbody>
+  </table>
+</div>
+</div>
+
+</div>
+<div class="shadow-box" >
+  <div class="product-table todo">
     <table>
       <thead>
         <tr>
-          <th>Comprado</th>
           <th>Nombre</th>
           <th>Descripción</th>
           <th>Precio</th>
          
         </tr>
       </thead>
-      <tbody>
-        <tr v-for="(product, index) in products" :key="index">
-          <td data-label="Comprado"><img :src="product.foto" alt="Foto del producto"></td>
-          <td data-label="Nombre">{{ product.nombre }}</td>
-          <td data-label="Descripción">{{ product.descripcion }}</td>
-          <td data-label="Precio">{{ product.precio }}</td>
+      <tbody v-for="data in dataCompras.data" :key="data.id">
+        <tr>
+          <td>{{ data.idbuy }}</td>
+          <td>{{ data.userid }}</td>
+          <td>{{ data.productid }}</td>
           
         </tr>
       </tbody>
     </table>
   </div>
 </div>
+</template>
 
-</div>
-<div class="shadow-box" >
-    <div class="product-table todo">
-      <table>
-        <thead>
-          <tr>
-            <th>Vendido</th>
-            <th>Nombre</th>
-            <th>Descripción</th>
-            <th>Precio</th>
-           
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(product, index) in products" :key="index">
-            <td><img :src="product.foto" alt="Foto del producto"></td>
-            <td>{{ product.nombre }}</td>
-            <td>{{ product.descripcion }}</td>
-            <td>{{ product.precio }}</td>
-            
-          </tr>
-        </tbody>
-      </table>
-    </div>
-</div>
-  </template>
-  
-  <script setup>
-  import { ref } from 'vue';
-  
-  const products = ref([
-    {
-      foto: 'ruta/a/foto1.jpg',
-      nombre: 'Producto 1',
-      descripcion: 'Descripción del producto 1',
-      precio: 10.99
-    },
-    {
-      foto: 'ruta/a/foto2.jpg',
-      nombre: 'Producto 2',
-      descripcion: 'Descripción del producto 2',
-      precio: 19.99
-    },
-    // Agrega más productos aquí
-  ]);
-  
-  function editarProducto(index) {
-    // Lógica para editar el producto seleccionado
-  }
-  
-  function borrarProducto(index) {
-    // Lógica para borrar el producto seleccionado
-  }
-  </script>
-  
+<script setup>
+import { defineProps, ref } from 'vue'
+// import axios from 'axios'
+defineProps({
+dataCompras: Object
+})
+
+
+function editarProducto(index) {
+  // Lógica para editar el producto seleccionado
+}
+
+function borrarProducto(index) {
+  // Lógica para borrar el producto seleccionado
+}
+</script>
   <style>  
 
   
